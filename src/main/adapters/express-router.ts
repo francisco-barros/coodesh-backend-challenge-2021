a132b04ca74333d1a4c7189dc9e100f5889159fa
Francisco Barros <francisco-barros@bitbucket.org>
2021-12-28 20:24:56 -0300

    change express adapter returned error

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -6,6 +6,8 @@ type Adapter = (controller: Controller) => RequestHandler
 
 export const adaptExpressRoute: Adapter = controller => async (req: Request, res: Response) => {
   const { statusCode, data } = await controller.handle({ ...req.params, ...req.body, ...req.query })
-  const json = [200, 204, 201].includes(statusCode) ? data : { error: data }
+
+  const json = [200, 204, 201].includes(statusCode) ? data : { error: data.message }
+
   res.status(statusCode).json(json)
 }
