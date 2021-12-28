923cb02b5e02287583a43d9e0ebc9815613e5e77
Francisco Barros <francisco-barros@bitbucket.org>
2021-12-28 20:36:48 -0300

    move ArticleStubs to its own folder

---
+++ src/main/mocks/ArticleMocks.ts
@@ -0,0 +1,35 @@
+export const articlesStub = [{
+  featured: false,
+  title: 'any_title',
+  url: 'any_url',
+  imageUrl: 'any_imageUrl',
+  newsSite: 'any_newsSite',
+  summary: 'any_sumary',
+  publishedAt: new Date(),
+  launches: [{
+    id: 'any_launch_id',
+    provider: 'any_provider'
+  }],
+  events: [{
+    id: 'any_event_id',
+    provider: 'any_provider'
+  }]
+},
+{
+  featured: false,
+  title: 'other_title',
+  url: 'other_url',
+  imageUrl: 'other_imageUrl',
+  newsSite: 'other_newsSite',
+  summary: 'other_sumary',
+  publishedAt: new Date(),
+  launches: [{
+    id: 'other_launch_id',
+    provider: 'other_provider'
+  }],
+  events: [{
+    id: 'other_event_id',
+    provider: 'other_provider'
+  }]
+}
+]
--- src/main/routers/ListOneArticleRouter.spec.ts
+++ src/main/routers/ListOneArticleRouter.spec.ts
@@ -3,45 +3,10 @@ import request from 'supertest'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { ArticleModel } from '../../infra/mongo/models'
 import { app } from '../../app'
+import {articlesStub} from '../mocks'
 
 describe('ListOneArticleRouter', () => {
   let mongoServer: MongoMemoryServer
-
-  const articlesStub = [{
-    featured: false,
-    title: 'any_title',
-    url: 'any_url',
-    imageUrl: 'any_imageUrl',
-    newsSite: 'any_newsSite',
-    summary: 'any_sumary',
-    publishedAt: new Date(),
-    launches: [{
-      id: 'any_launch_id',
-      provider: 'any_provider'
-    }],
-    events: [{
-      id: 'any_event_id',
-      provider: 'any_provider'
-    }]
-  },
-  {
-    featured: false,
-    title: 'other_title',
-    url: 'other_url',
-    imageUrl: 'other_imageUrl',
-    newsSite: 'other_newsSite',
-    summary: 'other_sumary',
-    publishedAt: new Date(),
-    launches: [{
-      id: 'other_launch_id',
-      provider: 'other_provider'
-    }],
-    events: [{
-      id: 'other_event_id',
-      provider: 'other_provider'
-    }]
-  }
-  ]
 
   beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create()
