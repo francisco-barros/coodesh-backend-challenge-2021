c2f5fe3dd39899e0f79b4b1fd029f8936ff6e3cf
Francisco Barros <francisco-barros@bitbucket.org>
2021-12-28 15:19:53 -0300

    add express adapter

---
+++ src/main/adapters/express-router.ts
@@ -0,0 +1,11 @@
+import { Controller } from '../../application/controllers'
+
+import { RequestHandler, Request, Response } from 'express'
+
+type Adapter = (controller: Controller) => RequestHandler
+
+export const adaptExpressRoute: Adapter = controller => async (req: Request, res: Response) => {
+  const { statusCode, data } = await controller.handle({ ...req.params, ...req.body, ...req.query })
+  const json = [200, 204, 201].includes(statusCode) ? data : { error: data }
+  res.status(statusCode).json(json)
+}
