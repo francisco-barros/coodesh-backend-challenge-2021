b411632547c370a5ac92a093a39ac11f08cc1429
Francisco Barros <franciscobarros.dev@gmail.com>
2022-01-23 15:16:23 -0300

    add ListAll method to ListAllArticlesRepository on Domain Layer

--- src/domain/repositories/Article-repos.ts
+++ src/domain/repositories/Article-repos.ts
@@ -8,3 +8,12 @@ export namespace ListOneArticleRepository {
   export interface Input { id: number }
   export type Output = Article | null
 }
+
+export interface ListAllArticlesRepository {
+  listAll: (input: ListAllArticlesRepository.Input) => Promise<ListAllArticlesRepository.Output>
+}
+
+export namespace ListAllArticlesRepository {
+  export interface Input { page?: number, limit?: number }
+  export type Output = Article[] | null
+}
