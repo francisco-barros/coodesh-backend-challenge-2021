0945c094797edd6670facb95e521c79c930458d4
Francisco Barros <franciscobarros.dev@gmail.com>
2022-01-25 06:36:29 -0300

    add mongoose connection to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
 import { app } from './app'
 
-const PORT = 8080
-app.listen(PORT, () => { console.log(`Server running at port ${PORT}.`) })
+const MONGO_URI = process.env.MONGO_URI as string
+
+mongoose.connect(MONGO_URI)
+  .then(_result => app.listen(process.env.PORT, () => console.log(`Server running at port ${process.env.PORT}.`)))
+  .catch(err => console.log(err))
