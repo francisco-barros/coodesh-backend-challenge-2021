59a3f2212be7ec11f64958e05530be0e2f7a97a1
Francisco Barros <franciscobarros.dev@gmail.com>
2022-01-25 10:54:57 -0300

    add cronDatabaseUpdate function call to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
+import {cronDatabaseUpdate} from './database-seed/updateDatabaseCron'
 
 import { app } from './app'
 
 const MONGO_URI = process.env.MONGO_URI as string
 
 mongoose.connect(MONGO_URI)
-  .then(_result => app.listen(process.env.PORT, () => console.log(`Server running at port ${process.env.PORT}.`)))
+  .then(async _result => {
+    await cronDatabaseUpdate()
+    app.listen(process.env.PORT, () => console.log(`Server running at port ${process.env.PORT}.`))
+    }
+  )
   .catch(err => console.log(err))
