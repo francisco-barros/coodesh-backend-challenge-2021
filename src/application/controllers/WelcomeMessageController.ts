96033f193b9ac92cadc1361f2e058ffcedd9b5b0
Francisco Barros <franciscobarros.dev@gmail.com>
2022-01-25 02:16:35 -0300

    add WelcomeMessageController on Application layer

---
+++ src/application/controllers/WelcomeMessageController.ts
@@ -0,0 +1,20 @@
+import { Controller } from './controller'
+import { HttpResponse, ok, serverError } from '../helpers'
+
+type Output = Error | String
+
+export class WelcomeMessageController extends Controller {
+  constructor () {
+    super()
+  }
+
+  async handle (_request: any): Promise<HttpResponse<Output>> {
+    try {
+      const welcomeMessage = `Back-end Challenge 2021 🏅 - Space Flight News`
+
+      return ok(welcomeMessage)
+    } catch (error) {
+      return serverError(new Error('An error occurred, please try again later'))
+    }
+  }
+}
